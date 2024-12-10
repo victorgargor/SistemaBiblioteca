f6a4267af9379deb840e16d4b47b8fdb3cdbf21a
victorgargor <victor.gargor@educa.jcyl.es>
2024-12-10 12:47:47 +0100

    Primer Commit

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "https://biblioteca.victor.local/";
+const host = "192.168.3.202";
+const user = "userdbbiblioteca";
+const pass = "672qJ&btbWrKmtd%";
+const db = "bdbiblioteca";
+const charset = "charset=utf8";
+?>
